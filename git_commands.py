402c5807fb47c63c316d7a4333109647baa07322
312Avenue <djumagaziev026@mail.ru>
2022-06-02 11:01:52 +0600

    initial commit.

---
+++ git_commands.py
@@ -0,0 +1,12 @@
+git init -создает папку с расширением .git
+git remote add name url - вместо name url http или ssh ссылки, добавляет удаленный репорезоторий
+git pull origin master - стягивает изменения с ветки файлы
+git status - показывает статус проекта
+git add - добавляет файлы в рабочее пространство
+git commit -m "comment" - добавляет все команды находящиеся в индексе во нутреннюю команду
+git branch - менеджер веток, с помощью команды можно просматривать список веток
+gti branch name branch - создает новую ветку
+git checkout name branch - переключение веток
+git push name branch - оптравка кода в удаленный репозеторий
+git push origin master
+git reset filename - удаляет файл из индекса по имени файла
